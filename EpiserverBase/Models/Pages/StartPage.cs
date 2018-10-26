36ae24afaed02459163ede4f6fea62a822252c58
Maciej Mickiewicz <maciej.mickiewicz@bright-it.com>
2018-10-26 13:56:56 +0200

    Add first version of Epsierver base project

---
+++ EpiserverBase/Models/Pages/StartPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EPiServer.Web;
+
+namespace EpiserverBase.Models.Pages
+{
+    [ContentType(
+        GUID = "5f5a391f-6b6a-4659-8e40-67a1860c2e52",
+        GroupName = Global.GroupNames.Specialized)]
+    public class StartPage : SitePageData
+    {
+        [Display(
+            Name = "Main body",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        [CultureSpecific]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
